c44e011a561643a3d788731f8d78e86084a9cd17
david raygoza <david_raygoza@hotmail.com>
2012-12-16 23:12:53 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diectorio::Application.config.secret_token = '8ab093bf97b7ca32b2d102df5ef37fc55ac4e5e2dc4ffdf754cd3dfe36eef3150a1934ddeb6062248c925c70b2e8126c77e9f1cdd555094a7770d9f938e5471d'
